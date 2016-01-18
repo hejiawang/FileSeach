873abfe6eae7da74879fd2a8e39247a3f6f4c4c3
hejiawang <952327407@qq.com>
2016-01-18 16:39:48 +0800

    列出计算机中的文件

--- src/test/java/com/run/wang/fileseach/utils/test/FileUtilTest.java
+++ src/test/java/com/run/wang/fileseach/utils/test/FileUtilTest.java
@@ -1,8 +1,8 @@
 package com.run.wang.fileseach.utils.test;
 
-import org.junit.Test;
+import java.io.File;
 
-import com.run.wang.fileseach.utils.FileUtil;
+import org.junit.Test;
 
 /**
  * 
@@ -11,8 +11,31 @@ import com.run.wang.fileseach.utils.FileUtil;
  */
 public class FileUtilTest {
 
+	private void showFileSons( File[] files  ){
+		if( files == null ){//如：cd驱动器有盘符，当没有光盘放入时，files为空，
+			return;
+		}
+		for( int j=0; j<files.length; j++ ){
+			File file = files[j];
+			if( file.isDirectory() ){
+				File[] filess = file.listFiles();
+				showFileSons(filess);
+			} else {
+				System.out.println(file.getAbsolutePath());
+			}
+		}
+	} 
+	
 	@Test
-	public void getFileRootsTest(){
-		FileUtil.getFileRoots();
+	public void test() {
+		
+		File[] fileRoots = File.listRoots();//得到计算机根目录，如：win系统中C:\  D:\  Linux系统中  \
+		for( int i=0; i<fileRoots.length; i++ ){
+			File fileRoot = fileRoots[i];	//  如   C:\
+			System.out.println(fileRoot.getAbsolutePath());
+			
+			File[] files = fileRoot.listFiles();
+			showFileSons(files);
+		}
 	}
 }
